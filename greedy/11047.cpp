14dc3be8f689dfd010f935e3bea22fbc78a30100
000wan <87213416+000wan@users.noreply.github.com>
2023-01-29 03:13:37 +0900

    Change dev setting

---
+++ greedy/11047.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    int  N;
+    cin >> ;
+
+    int N;
+    cin >> ;
+
+    return 0;
+}
