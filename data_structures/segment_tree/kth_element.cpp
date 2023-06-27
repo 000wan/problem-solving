d32558d4279a7ecdc245e76f68eed906d607cc60
000wan <87213416+000wan@users.noreply.github.com>
2023-06-28 00:15:12 +0900

    Solve segment tree problems

--- data_structures/segment_tree/kth_element.cpp
+++ data_structures/segment_tree/kth_element.cpp
@@ -7,21 +7,24 @@ struct SegmentTree {
     int size;
     vector<ll> tree, data;
     SegmentTree(int n):
-        size(n), tree(4*n), data(n) {}
+        size(n), tree(4*n, 0), data(n, 0) {}
     SegmentTree(int n, vector<ll> a):
         size(n), tree(4*n), data(a) {
             build();
         }
     public:
         void build() {
-            build(1, 0, size-1);
+            build(1, 1, size-1);
         }
         ll get(int l, int r) {
-            return get(1, 0, size-1, l, r);
+            return get(1, 1, size-1, l, r);
         }
         void update(int pos, ll val) {
-            data[pos] = val;
-            update(1, 0, size-1, pos, val);
+            data[pos] += val;
+            update(1, 1, size-1, pos, val);
+        }
+        int search(ll k) {
+            return search(1, 1, size-1, k);
         }
     private:
         void build(int v, int s, int e) {
@@ -30,43 +33,51 @@ struct SegmentTree {
             }
             int mid = (s+e)/2;
             build(2*v, s, mid); build(2*v+1, mid+1, e);
-            tree[v] = min(tree[2*v], tree[2*v+1]);
+            tree[v] = tree[2*v] + tree[2*v+1];
         }
         ll get(int v, int s, int e, int l, int r) {
-            if (e < l || r < s) return INF;
+            if (e < l || r < s) return 0;
             if (l <= s && e <= r) return tree[v];
             int mid = (s+e)/2;
-            return min(get(2*v, s, mid, l, r), get(2*v+1, mid+1, e, l, r));
+            return get(2*v, s, mid, l, r) + get(2*v+1, mid+1, e, l, r);
         }
         void update(int v, int s, int e, int pos, ll val) {
             if (s == e) {
-                tree[v] = val; return;
+                tree[v] += val; return;
             }
             int mid = (s+e)/2;
             if (pos <= mid) update(2*v, s, mid, pos, val);
             else update(2*v+1, mid+1, e, pos, val);
-            tree[v] = min(tree[2*v], tree[2*v+1]);
+            tree[v] = tree[2*v] + tree[2*v+1];
+        }
+        int search(int v, int s, int e, ll x) {
+            if (s == e) {
+                tree[v] -= 1; return s;
+            }
+            int mid = (s+e)/2, ret;
+            if (x <= tree[2*v])
+                ret = search(2*v, s, mid, x);
+            else
+                ret = search(2*v+1, mid+1, e, x-tree[2*v]);
+            tree[v] = tree[2*v] + tree[2*v+1];
+            return ret;
         }
 };
 
-
+const int MAX_N = 1e6;
 void solve() {
-    int N; cin >> N;
-    vector<ll> a(N);
-    for (auto& x : a) {
-        cin >> x;
-    }
-    int M; cin >> M;
+    int N;
+    cin >> N;
     
-    SegmentTree seg(N, a);
-    while (M--) {
+    SegmentTree seg(MAX_N+1); seg.build();
+    while (N--) {
         int mod; cin >> mod;
         if (mod == 1) {
-            int i, v; cin >> i >> v; i--;
-            seg.update(i, v);
+            int x; cin >> x;
+            cout << seg.search(x) << '\n';
         } else {
-            int i, j; cin >> i >> j; i--, j--;
-            cout << seg.get(i, j) << '\n';
+            int x, c; cin >> x >> c;
+            seg.update(x, c);
         }
     }
 }
